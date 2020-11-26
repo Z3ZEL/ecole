16652fcc1116d8004569db5bf76658a34a4e3029
Z3ZEL <66113155+Z3ZEL@users.noreply.github.com>
2020-11-26 23:30:43 +0100

    Update annale2016-2017.py

--- Info/CorrectionAnnale/annale2016-2017.py
+++ Info/CorrectionAnnale/annale2016-2017.py
@@ -9,16 +9,16 @@ def Peigne(img):
 
             if not(y%2):
                 pxColor = img.getpixel((x,y))
-                if x - shift < 0:
+                if x - shift < 0 :
                    newImage.putpixel(((3*w+4*x)//4,y),(255,255,255))
                 else:
                    newImage.putpixel((x-shift,y),pxColor)
             else:
-                pxColor = img.getpixel(((w-1)-x,y))
-                if (w-1)-x + shift >= w:
-                    newImage.putpixel(((-3*w+4*((w-1)-x))//4,y),(255,255,255))
+                pxColor = img.getpixel((x,y))
+                if x + shift >= w :
+                    newImage.putpixel(((-3*w+4*x)//4,y),(255,255,255))
                 else:
-                    newImage.putpixel(((w-1)-x+shift,y),pxColor)
+                    newImage.putpixel((x+shift,y),pxColor)
     return newImage
 
 testImg = Image.open("teapot.png")
